0ec217107f2eebcaaf6591d4fbd1efb36a6880ba
lucky7070 <luckysolanki.adiyogi@gmail.com>
2025-11-25 16:19:30 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::view('/contact', 'front.contact')->name('contact');
 Route::post('/contact', [FrontController::class, 'contactUs'])->name('contact');
 
 Route::view('/legal-services-ppc', 'front.legal-services-ppc')->name('legal-services-ppc');
+Route::view('/Supplier-Services', 'front.supplier-services')->name('supplier-services');
 
 Route::get('test', [CommonController::class, 'test'])->name('test');
 Route::get('{guard}', fn($guard) => redirect($guard == 'admin' ?  url('/admin/login') : url("/$guard/login")))->whereIn('guard', ['admin']);
